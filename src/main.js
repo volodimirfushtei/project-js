4a39650d8fac0999e2ddad82683ba5c22268591d
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-18 02:47:07 +0300

    add

--- src/main.js
+++ src/main.js
@@ -31,4 +31,43 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', debouncedCheckScroll);
 
   checkScroll();
+
+  function addDesktopItems() {
+    const container = document.querySelector('.cover-section .cover-container');
+
+    if (!document.querySelector('.cover-list.desktop')) {
+      const newUl = document.createElement('ul');
+      newUl.classList.add('cover-list', 'desktop');
+
+      const newLi = document.createElement('li');
+      newLi.innerHTML = `
+        <picture class="cover-picture">
+          <source srcset="./img/Rectangle-1-1x.jpg, ./img/Rectangle-1-2x.jpg" media="(min-width: 1158px)" />
+          <source srcset="./img/Rectangle-1-1x.jpg, ./img/Rectangle-1-2x.jpg" media="(min-width: 768px)" />
+          <source srcset="./img/Rectangle-1-1x.jpg, ./img/Rectangle-1-2x.jpg" media="(max-width: 767px)" />
+          <img src="./img/Rectangle-1-1x.jpg" alt="Cover 1" width="282" class="cover-img" />
+        </picture>
+      `;
+      newUl.appendChild(newLi);
+      const existingLists = container.querySelectorAll('.cover-list');
+      container.insertBefore(newUl, existingLists[0]);
+    }
+  }
+
+  function removeDesktopItems() {
+    const desktopList = document.querySelector('.cover-list.desktop');
+    if (desktopList) {
+      desktopList.remove();
+    }
+  }
+
+  function handleResize() {
+    if (window.innerWidth >= 1158) {
+      addDesktopItems();
+    } else {
+      removeDesktopItems();
+    }
+  }
+  window.addEventListener('load', handleResize);
+  window.addEventListener('resize', handleResize);
 });
