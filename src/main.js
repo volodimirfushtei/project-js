f18491cb347b8681230040fba82a38b549f6c87c
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-20 01:10:04 +0300

    add

--- src/main.js
+++ src/main.js
@@ -57,7 +57,12 @@ function addDesktopItems() {
     container.insertBefore(newUl, existingLists[0]);
   }
 }
-
+function removeDesktopItems() {
+  const desktopList = document.querySelector('.cover-list.desktop');
+  if (desktopList) {
+    desktopList.remove();
+  }
+}
 function handleResize() {
   if (window.innerWidth >= 1440) {
     addDesktopItems();
